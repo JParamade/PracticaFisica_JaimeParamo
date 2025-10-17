ebfee2c4951a72fd0d9a28490bb4034ab6165160
JParamade <jaime.paramo@outlook.com>
2025-10-17 18:45:15 +0200

    Project Imported

---
+++ Source/Physics/Weapons/HitscanWeaponComponent.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapons/HitscanWeaponComponent.h"
+#include <Kismet/KismetSystemLibrary.h>
+#include <Kismet/GameplayStatics.h>
+#include "PhysicsCharacter.h"
+#include "PhysicsWeaponComponent.h"
+#include <Camera/CameraComponent.h>
+#include <Components/SphereComponent.h>
+
+void UHitscanWeaponComponent::Fire()
+{
+	Super::Fire();
+
+  if (!IsValid(Character) || IsValid(Character->FirstPersonCameraComponent)) return;
+
+  FVector vStart = GetComponentLocation();
+  FVector vForwardVector = Character->FirstPersonCameraComponent->GetForwardVector();
+  FVector vEnd = vStart + (vForwardVector * m_fRange);
+
+  FHitResult oHitResult;
+  FCollisionQueryParams oParams;
+  if (GetWorld()->LineTraceSingleByChannel(oHitResult, vStart, vEnd, ECC_Visibility, oParams)) {
+    AActor* pOtherActor = oHitResult.GetActor();
+    //ApplyDamage(pOtherActor, oHitResult);
+    onHitscanImpact.Broadcast(pOtherActor, oHitResult.ImpactPoint, vForwardVector);
+  }
+}
